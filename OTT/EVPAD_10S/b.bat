9025d59e04995807cde3446712e1ae380965b484
xiaolaba <xiao_laba_cn@yahoo.com>
2023-10-30 14:24:01 +0800

    Add files via upload

--- OTT/EVPAD_10S/b.bat
+++ OTT/EVPAD_10S/b.bat
@@ -1,6 +1,8 @@
-copy "%1" "%cd%/myTVsuper_CH+.wav"
+copy "%1" "%cd%\myTVsuper_CH+.wav"
 mir.exe myTVsuper_CH+.wav
-ren myTVsuper_CH+_filtered.wav %~n1_filtered.wav
-ren myTVsuper_CH+_modulated.wav %~n1_modulated.wav
+copy /y "%cd\%myTVsuper_CH+_filtered.wav" "%cd%\%~n1_filtered.wav"
+copy /y "%cd\%myTVsuper_CH+_modulated.wav" "%cd%\%~n1_modulated.wav"
+
+del "%cd%\myTVsuper*.wav"
 
 pause
