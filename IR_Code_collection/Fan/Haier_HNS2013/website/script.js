69af3d4604d4c6d5465bc684c3a3da16d879bc43
xiaolaba <xiao_laba_cn@yahoo.com>
2024-02-20 23:21:25 +0800

    button layout
    layout

--- IR_Code_collection/Fan/Haier_HNS2013/website/script.js
+++ IR_Code_collection/Fan/Haier_HNS2013/website/script.js
@@ -1,12 +1,24 @@
 function playAudio(audioFileName) {
+
   var audio = new Audio(audioFileName);
   audio.play();
 
+/*}
+    var audio = document.getElementById(audioFileName);
+  if (audio) {
+    audio.play();
+  } else {
+    console.error("Audio element not found.");
+  }
+*/  
+  
+/*
 // Remove the information div after a few seconds
 setTimeout(function() {
   var infoDiv = document.getElementById('info');
   infoDiv.parentNode.removeChild(infoDiv);
 }, 5000); // 5000 milliseconds = 5 seconds
+*/
 
 }
 
